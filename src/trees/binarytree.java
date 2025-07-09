73eb3c3627a73d542d81f7169f34978e17651f9e
bablu42089 <bablubabluranga@gmail.com>
2025-07-09 22:21:54 +0530

    hi

--- src/trees/binarytree.java
+++ src/trees/binarytree.java
@@ -58,10 +58,33 @@ public void display(Node root,String indent)
     display(root.right,"\t");
 
 }
+public  void pd()
+{
+    pd(root,0);
+}
+void pd(Node node,int level)
+{
+    if(node==null)
+    {
+        return;
+    }
+    pd(node.right,level+1);
+    if(level!=0)
+    {
+        for (int i = 0; i < level-1; i++) {
+            System.out.print("|\t\t");
+        }
+        System.out.println("|------>"+node.value);
+    }
+    else {
+        System.out.println(node.value);
+    }
+    pd(node.left,level+1);
+}
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         binarytree t=new binarytree();
         t.populate(sc);
-        t.display();
+        t.pd();
     }
 }
